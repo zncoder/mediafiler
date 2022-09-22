55947e328cdc18f9f210a08bc5dedfa21221524c
Bin Zhang <zncoder@gmail.com>
2022-09-21 22:50:22 -0700

    implement copyLink with copy command

--- asset/index.js
+++ asset/index.js
@@ -1,6 +1,15 @@
 async function copyLink(ev) {
   ev.preventDefault()
-  let a = ev.target.parentElement.querySelector('a')
-  await navigator.clipboard.writeText(a.href)
-  console.log(a.href)
+  let url = ev.target.parentElement.querySelector('a').href
+
+  let ta = document.createElement("textarea")
+  ta.textContent = url
+  ta.style.position = "fixed"
+  document.body.appendChild(ta)
+  ta.select()
+  try {
+    document.execCommand("copy")
+  } finally {
+    document.body.removeChild(ta);
+  }
 }
