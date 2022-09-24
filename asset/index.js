266a103e7f1b9c1667f5a6f8abcdb47d18749ea7
Bin Zhang <zncoder@gmail.com>
2022-09-23 23:52:25 -0700

    delete and undelete

--- asset/index.js
+++ asset/index.js
@@ -1,4 +1,4 @@
-async function copyLink(ev) {
+function copyLink(ev) {
   ev.preventDefault()
   let url = ev.target.parentElement.querySelector('a').href
 
@@ -11,5 +11,27 @@ async function copyLink(ev) {
     document.execCommand("copy")
   } finally {
     document.body.removeChild(ta);
+  }
+}
+
+function deleteFile(ev) {
+  ev.preventDefault()
+  let el = ev.target
+  let url = el.href
+  let lnk = el.parentElement.querySelector('a')
+  if (lnk.deleted) {
+    console.log('undelete')
+    delete lnk['deleted']
+    lnk.style.removeProperty('text-decoration')
+    lnk.style.removeProperty('background-color')
+
+    fetch(url + "?undelete")
+  } else {
+    console.log('delete')
+    lnk.deleted = 'yes'
+    lnk.style.textDecoration = 'line-through'
+    lnk.style.backgroundColor = 'DimGray'
+
+    fetch(url)
   }
 }
--- mediafiler.go
+++ mediafiler.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"embed"
+	"encoding/base32"
 	"flag"
 	"fmt"
 	"html/template"
@@ -16,15 +18,12 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
-type FilePath struct {
-	Name string
+type FileInfo struct {
 	Path string
-}
-
-type Files struct {
-	Files []FilePath
+	ID   string
 }
 
 type Dirs struct {
@@ -32,7 +31,8 @@ type Dirs struct {
 	suffixes []string
 
 	mu    sync.Mutex
-	files []string
+	files []FileInfo
+	toDel map[string]time.Time // path -> time deleted
 }
 
 //go:embed asset
@@ -56,6 +56,9 @@ func extractTitle(p string) string {
 }
 
 func (ds *Dirs) Index(w http.ResponseWriter, r *http.Request) {
+	ds.mu.Lock()
+	defer ds.mu.Unlock()
+
 	ds.refresh()
 
 	var bb bytes.Buffer
@@ -66,11 +69,76 @@ func (ds *Dirs) Index(w http.ResponseWriter, r *http.Request) {
 	w.Write(bb.Bytes())
 }
 
-func (ds *Dirs) refresh() {
+func (ds *Dirs) DeleteFile(w http.ResponseWriter, r *http.Request) {
 	ds.mu.Lock()
 	defer ds.mu.Unlock()
 
-	var files []string
+	id := filepath.Base(r.URL.Path)
+	_, undelete := r.URL.Query()["undelete"]
+
+	var filename string
+	for _, fi := range ds.files {
+		if fi.ID == id {
+			filename = fi.Path
+			break
+		}
+	}
+
+	if filename == "" {
+		http.Error(w, "Invalid id to delete", http.StatusBadRequest)
+		return
+	}
+
+	from := filename
+	to := filename + ".del"
+	if undelete {
+		from, to = to, from
+	}
+
+	err := os.Rename(from, to)
+	if err != nil {
+		http.Error(w, "Rename error", http.StatusInternalServerError)
+		return
+	}
+
+	if undelete {
+		delete(ds.toDel, from)
+	} else {
+		ds.toDel[to] = time.Now()
+	}
+}
+
+func (ds *Dirs) runDeleter() {
+	threshold := 11 * time.Minute
+	for now := range time.Tick(threshold) {
+		stayTs := now.Add(-threshold)
+
+		var todel []string
+		ds.mu.Lock()
+		for p, ts := range ds.toDel {
+			if ts.Before(stayTs) {
+				delete(ds.toDel, p)
+				todel = append(todel, p)
+			}
+		}
+		ds.mu.Unlock()
+
+		for _, p := range todel {
+			err := os.Remove(p)
+			if err != nil {
+				log.Printf("delete %s err:%v", p, err)
+			}
+		}
+	}
+}
+
+func sha(name string) string {
+	b := sha256.Sum224([]byte(name))
+	return base32.StdEncoding.EncodeToString(b[:])[:16]
+}
+
+func (ds *Dirs) refresh() {
+	var files []FileInfo
 	for _, d := range ds.dirs {
 		fs.WalkDir(os.DirFS(d), ".", func(p string, _ fs.DirEntry, err error) error {
 			if err != nil {
@@ -78,7 +146,8 @@ func (ds *Dirs) refresh() {
 			}
 			for _, sfx := range ds.suffixes {
 				if strings.HasSuffix(p, sfx) {
-					files = append(files, filepath.Join(d, p))
+					p := filepath.Join(d, p)
+					files = append(files, FileInfo{Path: p, ID: sha(p)})
 					break
 				}
 			}
@@ -87,8 +156,8 @@ func (ds *Dirs) refresh() {
 	}
 
 	sort.Slice(files, func(i, j int) bool {
-		di, fi := filepath.Split(files[i])
-		dj, fj := filepath.Split(files[j])
+		di, fi := filepath.Split(files[i].Path)
+		dj, fj := filepath.Split(files[j].Path)
 		if fi == fj {
 			return di < dj
 		}
@@ -110,7 +179,7 @@ func (ds *Dirs) ServeFile(w http.ResponseWriter, r *http.Request) {
 	fid, _ := strconv.Atoi(ms[1])
 
 	ds.mu.Lock()
-	filename := ds.files[fid]
+	filename := ds.files[fid].Path
 	ds.mu.Unlock()
 	if filename == "" {
 		http.Error(w, "Not found", http.StatusNotFound)
@@ -136,10 +205,13 @@ func main() {
 	dirs := Dirs{
 		dirs:     flag.Args(),
 		suffixes: suffixes,
+		toDel:    make(map[string]time.Time),
 	}
+	go dirs.runDeleter()
 
 	http.HandleFunc("/", dirs.Index)
 	http.HandleFunc("/f/", dirs.ServeFile)
+	http.HandleFunc("/delete/", dirs.DeleteFile)
 	http.Handle("/asset/", http.FileServer(http.FS(asset)))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *portFlag), nil))
 }
